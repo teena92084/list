58c3721948ea61ecc4ba8650833ee9fd48c51bba
teena92084 <103328068+teena92084@users.noreply.github.com>
2022-08-27 20:39:28 +0530

    Add files via upload

---
+++ l.doc.Q.Speart charcte and  numeric value.py
@@ -0,0 +1,32 @@
+# a=[1,"f",2,"k",5,"u","p",7,"r"]
+# b=[]
+# c=[]
+# i=0
+# while i<len(a):
+#     if type(a[i])==str:
+#         b.append(a[i])
+#     else:
+#         c.append(a[i])
+#     i=i+1
+# print(b)
+# print(c)            
+
+
+
+
+# a=["a"],["k","a","n"],["a","w","a"]
+# i=0
+# b=[]
+# while i<len(a):
+#     j=0
+#     while j<len(a):
+#         b.append(a[i])
+#         j=j+1
+#     i=i+1
+# b="".join(b) 
+# print(b)       
+
+
+# #     b.append(list(a[i]))
+# #     i=i+1
+# # print(b)    
