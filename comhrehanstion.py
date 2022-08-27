718b7a4a75c1ff7ee2cc363c6a793bd18466633e
teena92084 <103328068+teena92084@users.noreply.github.com>
2022-08-27 20:36:30 +0530

    Add files via upload

---
+++ comhrehanstion.py
@@ -0,0 +1,34 @@
+# a=[1,2,3,4]
+# b=[]
+# for i in a:
+#     b.append(i)
+# print(b)
+ 
+
+# a=[[1,2],[3,2],[3,4,5],[1,4]]
+# sum=0
+# i=0
+# b=[]
+# while i<len(a):
+#     j=0
+#     s=0
+#     while j<len(a[i]):
+#         s=s+a[i][j]
+#         j=j+1
+#     sum=sum+s
+#     i=i+1
+# print(sum)        
+
+
+# a=[[12],[2,4],[2]]
+# i=0
+# sum=0
+# while i<len(a):
+#     j=0
+#     s=0
+#     while j<len(a[i]):
+#         s=s+(a[i][j])
+#         j=j+1
+#     sum=sum+s
+#     i=i+1
+# print(sum)        
