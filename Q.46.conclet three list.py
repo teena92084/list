024fad188c65e9d3cddb4e1e9dea1b5eaf0931e6
teena92084 <103328068+teena92084@users.noreply.github.com>
2022-08-27 20:46:19 +0530

    Add files via upload

---
+++ Q.46.conclet three list.py
@@ -0,0 +1,10 @@
+# a=['0', '1', '2', '3', '4']
+# b=['red', 'green', 'black', 'blue', 'white']
+# c=['100', '200', '300', '400', '500']
+# i=0
+# g=[]
+# while i<len(a):
+#     k=(a[i]+b[i]+c[i])
+#     g.append(k)
+#     i=i+1
+# print(g)    
