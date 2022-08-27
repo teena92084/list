7987ae477cacd02051d50bb7562d07986951c3a2
teena92084 <103328068+teena92084@users.noreply.github.com>
2022-08-27 20:54:44 +0530

    Add files via upload

---
+++ 000 or number ko alag krna.py
@@ -0,0 +1,26 @@
+
+
+# list=[2,4,1,0,2,3,0,3,0,4,0,7,0,]
+# i=0
+# a=[]
+# b=[]
+# while i<len(list):
+#     if list[i]==0:
+#         a.append(list[i])
+#     else:
+#         b.append(list[i])
+        
+#     i=i+1
+# print(a)
+# print(b)
+# print(b+a)
+
+
+# a=int(input("entr the number"))
+# i=1
+# b=[]
+# while i<=10:
+#     d=a*i
+#     b.append(d)
+#     i=i+1
+# print(b)     
