513d5fbf40f7c44cba09e8425ccf33ba62bd037e
teena92084 <103328068+teena92084@users.noreply.github.com>
2022-08-27 20:39:02 +0530

    Add files via upload

--- l.doc.prime number.py
+++ l.doc.prime number.py
@@ -32,28 +32,31 @@
 #     i-=1
 
 
-i=1
-l=[]
-while i<=100:
-    c=0
-    j=1
-    while j<=i:
-        if i%j==0:
-            c+=1
-        j=j+1
-    if c==2:
-        l.append(i)
-    i=i+1
-print(l)                
+# i=1
+# l=[]
+# while i<=100:
+#     c=0
+#     j=1
+#     while j<=i:
+#         if i%j==0:
+#             c+=1
+#         j=j+1
+#     if c==2:
+#         l.append(i)
+#     i=i+1
+# print(l)                
 
-i=1
-while i<=100:
-    c=0
-    j=1
-    while j<=i:
-        if i%j==0:
-            c=c+1
-        j=j+1
-    if c==2:
-        print(i,"prime")        
-    i=i+1    
+
+
+
+# i=1
+# while i<=100:
+#     c=0
+#     j=1
+#     while j<=i:
+#         if i%j==0:
+#             c=c+1
+#         j=j+1
+#     if c==2:
+#         print(i,"prime")        
+#     i=i+1    
