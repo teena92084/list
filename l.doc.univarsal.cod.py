3512940e78cdf1bff2147aed434343f068516fb5
teena92084 <103328068+teena92084@users.noreply.github.com>
2022-08-27 20:39:56 +0530

    Add files via upload

---
+++ l.doc.univarsal.cod.py
@@ -0,0 +1,16 @@
+# a=[[1,2,3,],4,[5,6,7],8,[9,0,10],11,[5,7,8]]
+# i=0
+# b=[]
+# c=[]
+# while i<len(a):
+#     if type(a[i])==list:
+#         b.append(a[i])
+#     else:
+#         c.append([a[i]])
+#     i=i+1        
+# # print(b) 
+# # print(c)
+# for i in range(len(c)):
+#     b[i].extend(c[i])
+# print(b)    
+
