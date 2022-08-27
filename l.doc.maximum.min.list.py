b7c92ef83dd46489573b0a26db4870225cd186ed
teena92084 <103328068+teena92084@users.noreply.github.com>
2022-08-27 20:38:38 +0530

    Add files via upload

---
+++ l.doc.maximum.min.list.py
@@ -0,0 +1,16 @@
+# a=[[4],[4,5,6],[7,8,9,3,4]]
+# i=0
+# max=0
+# min=1
+# k=[]
+# n=[]
+# while i<len(a):
+#     if max<len(a[i]):
+#         max=len(a[i])
+#         n=a[i]
+#     if min>=len(a[i]):
+#         min=len(a[i])
+#         k=a[i]
+#     i=i+1
+# print(max,n) 
+# print(min,k)
